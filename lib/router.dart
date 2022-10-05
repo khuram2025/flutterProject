377fc10303ec038ad85d0bea697d3d15eaafbcd3
khuram2025 <khuram2025@gmail.com>
2022-10-05 19:19:26 +0300

    startapp Foofie

---
+++ lib/router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:foodie/pages/root_app.dart';
+
+
+Route<dynamic> generateRoute(RouteSettings setting) {
+  final Map<String, dynamic> args = new Map<String, dynamic>();
+
+  switch (setting.name) {
+    case '/root_app':
+      return MaterialPageRoute(builder: (context) => RootApp());
+    default:
+      return MaterialPageRoute(
+          builder: (context) => Scaffold(
+                body: Center(),
+              ));
+  }
+}
